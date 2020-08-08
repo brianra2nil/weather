009890eff498ba68ecfffb43f94c131bc5553dad
brian <brianra2nil@hotmail.com>
2020-08-07 18:27:03 -0700

    updated

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 This weather app is designed for the user to type in a city and recieve today's weather as well as the 5 day forecast.
 A special highlighted results for UV index is also generated with a color code depending on the severity.
 
-A list of previously searched cities is recorded.  Future functionality will include being able to click on the previously searched cities for results.
+A list of previously searched cities is recorded.  If you click on a previously searched city you will get the results as well.
 
 Deployed site is:
 https://brianra2nil.github.io/weather/
--- app.js
+++ app.js
@@ -3,158 +3,162 @@ document.getElementById("yellow").style.backgroundColor = "yellow"
 document.getElementById("red").style.backgroundColor = "red"
 
 
-        let searches = JSON.parse(localStorage.getItem('searches')) || []
-        let uvResulter = 0
+let searches = JSON.parse(localStorage.getItem('searches')) || []
+let uvResulter = 0
 
 
-       
-        for (let i = 0; i< searches.length; i++) {
-            let itemElem = document.createElement('li')
-            itemElem.className = 'resultCity'
-            itemElem.innerHTML = `
+
+for (let i = 0; i < searches.length; i++) {
+    let itemElem = document.createElement('li')
+    itemElem.className = 'resultCity'
+    itemElem.innerHTML = `
             ${searches[i].text}
             `
-            document.getElementById('previous').append(itemElem)
+    document.getElementById('previous').append(itemElem) //this is the line that renders on screen
 
-        }
+}
+// new code from tutor, showing how to target classlist and call the api
+document.addEventListener('click', (event) => {
+   
+    if (event.target.classList.contains('resultCity')) {
+console.log('you clicked on a result city')
+let clickedCity = event.target.innerHTML.trim()
+console.log(clickedCity)
 
-        document.getElementById('button').addEventListener('click', event => {
-            event.preventDefault()
-            console.log('clicked')
-            
+api(clickedCity)
+    }
+})
 
-            let city = document.getElementById('input').value
-            console.log(city)
-
-            let cityObj = {
-                        text: city
-                    }
-                    searches.push(cityObj)
-                    localStorage.setItem('searches', JSON.stringify(searches))
-
-                    let itemElem = document.createElement('li')
-            itemElem.className = 'resultCity'
-            itemElem.innerHTML = `
-            ${city}
-            `
-            document.getElementById('previous').append(itemElem)
+document.getElementById('button').addEventListener('click', event => {
+    event.preventDefault()
+    console.log('clicked')
 
 
-                    // let prev = JSON.parse(localStorage.getItem('searches'))
-                    // document.getElementById('previous').innerHTML = `
-                    // <li> ${prev} </li>
-                    // `
-                   
-                    // location.reload()
+    let city = document.getElementById('input').value
+    console.log(city)
 
-                    
+    let cityObj = {
+        text: city
+    }
+    searches.push(cityObj)
+    localStorage.setItem('searches', JSON.stringify(searches))
 
-          
+    let itemElem = document.createElement('li')
+    itemElem.className = 'resultCity'
+    itemElem.innerHTML = `${city}`
+    document.getElementById('previous').append(itemElem)
 
-            axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=1dd25ac798a84daed3b612ef4b3c9a3e`)
-                .then(res => {
-                    console.log(res.data)
+    api(city);
 
-                    let forecast = res.data.list
+ 
 
-                   
-                    document.getElementById('result').innerHTML = `
-       <div>
-            <h3 id="searchedCity" class="alert alert-primary" role="alert">${res.data.name}</h1>
-            <h3 id="date">${moment().format('l')}</h2>
-        </div>
+})
+
+
+// we created a function for the api and display of data so that we can just call it instead of typing it twice.  Function is named "api" and used on lines 28 and 51 
+function api(city) {
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=1dd25ac798a84daed3b612ef4b3c9a3e`)
+        .then(res => {
+            console.log(res.data)
+
+            let forecast = res.data.list
+
+
+            document.getElementById('result').innerHTML = `
+<div>
+    <h3 id="searchedCity" class="alert alert-primary" role="alert">${res.data.name}</h1>
+    <h3 id="date">${moment().format('l')}</h2>
+</div>
 <div>
 <p id="today">
 <ul class="list-group">
-  <li id="today-weather">Weather: ${res.data.weather[0].description}</li>
-  <li id="today-temp">Temperature: ${res.data.main.temp}</li>
-  <li id="today-humidity">Humidity: ${res.data.main.humidity}</li>
-  <li id="today-wind">Wind Speed: ${res.data.wind.speed}</li>
-  <li id="today-coordinatesLon">Longitude: ${res.data.coord.lon}</li>
-  <li id="today-coordinatesLat">Latitude: ${res.data.coord.lat}</li>
- </ul>
+<li id="today-weather">Weather: ${res.data.weather[0].description}</li>
+<li id="today-temp">Temperature: ${res.data.main.temp}</li>
+<li id="today-humidity">Humidity: ${res.data.main.humidity}</li>
+<li id="today-wind">Wind Speed: ${res.data.wind.speed}</li>
+<li id="today-coordinatesLon">Longitude: ${res.data.coord.lon}</li>
+<li id="today-coordinatesLat">Latitude: ${res.data.coord.lat}</li>
+</ul>
 </p>
 </div>
 `
-                    let lon = res.data.coord.lon
-                    let lat = res.data.coord.lat
+            let lon = res.data.coord.lon
+            let lat = res.data.coord.lat
+
+
+            axios.get(`https://api.openweathermap.org/data/2.5/uvi?appid=1dd25ac798a84daed3b612ef4b3c9a3e&lat=${lat}&lon=${lon}`)
+                .then(res => {
+                    console.log(res.data)
+
+                    event.preventDefault()
+                    let uv = res.data.value
+                    uvResulter = uv
+                    document.getElementById('uvIndex').innerHTML = `
                    
-
-                    axios.get(`https://api.openweathermap.org/data/2.5/uvi?appid=1dd25ac798a84daed3b612ef4b3c9a3e&lat=${lat}&lon=${lon}`)
-                        .then(res => {
-                            console.log(res.data)
-
-                            event.preventDefault()
-                            let uv = res.data.value
-                            uvResulter = uv
-                            document.getElementById('uvIndex').innerHTML = `
-                           
-                            <div>
+                    <div>
 <p id="today">
 <ul class="list-group">
-  <li id="today-weather">UV Index: ${res.data.value}</li>
- 
- </ul>
+<li id="today-weather">UV Index: ${res.data.value}</li>
+
+</ul>
 </p>
 </div>
 
-                   
-                    `
-                            if (uv >= 0 && uv <= 2) { document.getElementById("uvIndex").style.backgroundColor = "green" }
-                            else if (uv >= 3 && uv <= 5) { document.getElementById("uvIndex").style.backgroundColor = "yellow" }
-                            else { document.getElementById("uvIndex").style.backgroundColor = "red" }
-
-                  
-
-
-
-                        })
-                        .catch(err => { console.log(err) })
-
-
-
-
-
-                })
-                .catch(err => { console.log(err) })
-
-
-
-
-
-            axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=1dd25ac798a84daed3b612ef4b3c9a3e`)
-                .then(res => {
-                    let forecast = res.data.list
-                   
-                    document.getElementById('forecast').innerHTML = ''
-
            
-                    for (let i = 5; i < forecast.length; i += 8) {
-
-                        let forecastElem = document.createElement('div')
-                        forecastElem.className = 'class="alert alert-warning" role="alert"'
-                        forecastElem.id = `day${i}`
-
-                        forecastElem.innerHTML = `
-        <div class="alert alert-warning" role="alert">
-          <h5 id="day${i}-day" class="card-title day${i}-day">${moment(forecast[i].dt_txt).format('dddd')}, ${moment(forecast[i].dt_txt).format('l')}</h5>
-          <h6 id="day${i}-weather" class="card-subtitle mb-2 text-black">Weather: ${forecast[i].weather[0].description}</h6>
-          <div class="card-text">
-            <p id="day${i}-icon" class="text-center"><img src="http://openweathermap.org/img/w/${forecast[i].weather[0].icon}.png"</p>
-            <p id="day${i}-temp">Temperature: ${forecast[i].main.temp}</p>
-            <p id="day${i}-humidity">Humidity: ${forecast[i].main.humidity}</p>
-            <p id="day${i}-wind">Wind Speed: ${forecast[i].wind.speed}</p>
-          </div>
-        </div>
-        <p> </p>
-        `
+            `
+                    if (uv >= 0 && uv <= 2) { document.getElementById("uvIndex").style.backgroundColor = "green" }
+                    else if (uv >= 3 && uv <= 5) { document.getElementById("uvIndex").style.backgroundColor = "yellow" }
+                    else { document.getElementById("uvIndex").style.backgroundColor = "red" }
 
 
-                        document.getElementById('forecast').append(forecastElem)
-                    }
+
+
 
                 })
                 .catch(err => { console.log(err) })
+
+
+
+
+
         })
+        .catch(err => { console.log(err) })
 
 
+
+
+
+    axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=1dd25ac798a84daed3b612ef4b3c9a3e`)
+        .then(res => {
+            let forecast = res.data.list
+
+            document.getElementById('forecast').innerHTML = ''
+
+
+            for (let i = 5; i < forecast.length; i += 8) {
+
+                let forecastElem = document.createElement('div')
+                forecastElem.className = 'class="alert alert-warning" role="alert"'
+                forecastElem.id = `day${i}`
+
+                forecastElem.innerHTML = `
+<div class="alert alert-warning" role="alert">
+  <h5 id="day${i}-day" class="card-title day${i}-day">${moment(forecast[i].dt_txt).format('dddd')}, ${moment(forecast[i].dt_txt).format('l')}</h5>
+  <h6 id="day${i}-weather" class="card-subtitle mb-2 text-black">Weather: ${forecast[i].weather[0].description}</h6>
+  <div class="card-text">
+    <p id="day${i}-icon" class="text-center"><img src="http://openweathermap.org/img/w/${forecast[i].weather[0].icon}.png"</p>
+    <p id="day${i}-temp">Temperature: ${forecast[i].main.temp}</p>
+    <p id="day${i}-humidity">Humidity: ${forecast[i].main.humidity}</p>
+    <p id="day${i}-wind">Wind Speed: ${forecast[i].wind.speed}</p>
+  </div>
+</div>
+<p> </p>
+`
+
+
+                document.getElementById('forecast').append(forecastElem)
+            }
+
+        })
+        .catch(err => { console.log(err) })
+}
